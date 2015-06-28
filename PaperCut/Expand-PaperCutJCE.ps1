3137d6d2377bf94f4b52f16ce5e5c79f61df229c
n8felton <n8felton@gmail.com>
2015-06-28 17:28:19 -0400

    Adding Expand-PaperCutJCE.ps1
    This script allows you to "install" the Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files to the embedded JRE that PaperCut uses.

---
+++ PaperCut/Expand-PaperCutJCE.ps1
@@ -0,0 +1,47 @@
+﻿Param
+(
+    # Path to JCE zip
+    [Parameter(Mandatory=$true,
+                Position=0)]
+    [string]
+    $JCEZipFile
+)
+
+$PaperCutPath         = "C:\Program Files\PaperCut MF"
+$RuntimeLibSecurity   = "$PaperCutPath\runtime\jre\lib\security", "$PaperCutPath\runtime\win64\jre\lib\security"
+$PolicyFiles          = "local_policy.jar", "US_export_policy.jar"
+
+
+Function Start-PolicyBackup
+{
+    $RuntimeLibSecurity | % {
+        $RuntimeLibSecurity = $_
+        $PolicyFiles | % {
+            Move-Item -Path $RuntimeLibSecurity\$_ `
+                      -Destination $RuntimeLibSecurity\$_.bak `
+                      -ErrorAction SilentlyContinue
+        }
+    }
+}
+
+# http://stackoverflow.com/questions/24672560/most-elegant-way-to-extract-a-directory-from-a-zipfile-using-powershell
+Function Expand-JCEZipFile ($JCEZipFile)
+{
+    $JCEZipFile = $JCEZipFile -replace '"',""
+    [IO.Compression.ZipFile]::OpenRead($JCEZipFile).Entries | ? {
+        $_.FullName -like "*.jar"
+    } | % {
+        $ZipFile = $_
+        $RuntimeLibSecurity | % {
+            $RuntimeLibSecurity = $_
+            $PolicyFiles | % {
+                $DestinationFile = "$RuntimeLibSecurity\$_"
+                [IO.Compression.ZipFileExtensions]::ExtractToFile($ZipFile, $DestinationFile, $true)
+            }
+        }
+        
+    }
+}
+
+Start-PolicyBackup
+Expand-JCEZipFile $JCEZipFile
